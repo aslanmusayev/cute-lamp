d3ab76d83cf1f113fb4df9ba0f5d04fbade952a2
aslanmusayev <aslanmusayev15@gmail.com>
2025-12-09 20:11:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const lamp = document.querySelector(".lamp");
 const light = document.querySelector(".lamp__light");
 
+let lampOn = false;
+
 lamp.addEventListener("click", () => {
-    light.style.opacity = light.style.opacity === "1" ? "0" : "1";
+    lampOn = !lampOn;
+
+    if (lampOn) {
+        light.style.opacity = "1";
+        lamp.classList.add("on");
+    } else {
+        light.style.opacity = "0";
+        lamp.classList.remove("on");
+    }
 });
