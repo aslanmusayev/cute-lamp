e7c4cf5a044081c1147eea22c74771f330f6a2b7
aslanmusayev <aslanmusayev15@gmail.com>
2025-12-09 20:27:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const lamp = document.querySelector(".lamp");
-const light = document.querySelector(".lamp__light");
 
-let lampOn = false;
+let isOn = false;
 
 lamp.addEventListener("click", () => {
-    lampOn = !lampOn;
+    isOn = !isOn;
 
-    if (lampOn) {
-        light.style.opacity = "1";
-        lamp.classList.add("on");
+    if (isOn) {
+        lamp.style.setProperty("--on", 1);
+        lamp.style.setProperty("--cord", "yellow");
     } else {
-        light.style.opacity = "0";
-        lamp.classList.remove("on");
+        lamp.style.setProperty("--on", 0);
+        lamp.style.setProperty("--cord", "#ccc");
     }
 });
